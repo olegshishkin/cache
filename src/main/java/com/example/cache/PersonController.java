33fac322e2755316e0eded19fa393775baecfec9
Oleg Shishkin <olegshishkin@gmail.com>
2020-02-02 18:03:37 +0300

    add logs for controller

--- src/main/java/com/example/cache/PersonController.java
+++ src/main/java/com/example/cache/PersonController.java
@@ -1,5 +1,6 @@
 package com.example.cache;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -7,9 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 public class PersonController {
 
+    public static final String FETCH_FROM_DB = "Fetch from DB";
     private final PersonService service;
 
     @Autowired
@@ -19,16 +22,19 @@ public class PersonController {
 
     @GetMapping("/person")
     public Person find(@RequestParam("name") String name) {
+        log.info(FETCH_FROM_DB);
         return service.findByName(name).orElse(null);
     }
 
     @GetMapping("/")
     public List<Person> getAll() {
+        log.info(FETCH_FROM_DB);
         return service.findAll();
     }
 
     @GetMapping("/add")
     public Person add(@RequestParam("name") String name, @RequestParam("age") int age) {
+        log.info(FETCH_FROM_DB);
         Person person = new Person();
         person.setName(name);
         person.setAge(age);
